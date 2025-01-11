6cddca231cef718cbe2b9d93d08af22d208da85e
lePizdon <bestsenpaii@gmail.com>
2025-01-11 17:30:26 +0300

    парочка DTO классов (один не нужен в текущей реализации, но пока не убираю), сервис для самого подсчёта и обновление репозитория и сервисов для нужных выборок

--- src/main/java/vet/goat/repository/payload/DosageRepository.java
+++ src/main/java/vet/goat/repository/payload/DosageRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import vet.goat.dto.DosageDetailsDTO;
 import vet.goat.dto.DosageRange;
 import vet.goat.entity.payload.Dosage;
 
